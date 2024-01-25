a9ad20a53ccf4842c61845d8f2da24730439c38c
Abdiel Flores <abdielfg@gmail.com>
2024-01-24 19:28:34 -0700

    React query, bun

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+
 import Navigation from './app/navigation/NavigationContainer'
+
+const queryClient = new QueryClient()
 
 export default function App() {
   return (
     <>
-      <Navigation />
+      <QueryClientProvider client={queryClient}>
+        <Navigation />
+      </QueryClientProvider>
     </>
   )
 }
